4b69f40ddcee5e0dbe733bb42fa6ee5a5149e4cf
Shaun <shaun37k@gmail.com>
2019-07-18 22:20:37 -0400

    slug generation

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,16 @@
-module.exports.onCreateNode = (node, actions) => {
+// generate a slug for each post
+
+// node.js docs: path.basename(path[, ext])
+const path = require('path')
+
+module.exports.onCreateNode = ({ node, actions }) => {
   const { createNodeField } = actions
 
-  // JSON.stringify instead of console.log(node) so that it can be viewed in terminal
-  console.log(JSON.stringify(node, undefined, 4))
+  if (node.internal.type === 'MarkdownRemark') {
+    const slug = path.basename(node.fileAbsolutePath, '.md')
+
+    // make sure slug prints in terminal (look for the noticeable '@@@@@@...')
+    console.log('@@@@@@@@@@@@@@@@@@', slug)
+    // console.log(JSON.stringify(node, undefined, 4))
+  }
 }
