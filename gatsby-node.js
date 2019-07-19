ace673f925e02347a3fb04bea2a94cf8c1bca7a9
Shaun <shaun37k@gmail.com>
2019-07-18 23:25:20 -0400

    render each blog post as its own page

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,7 @@
 // generate a slug for each post
 
 // node.js docs: path.basename(path[, ext])
+// gatsby docs: gatsby node api: onCreateNode
 const path = require('path')
 
 module.exports.onCreateNode = ({ node, actions }) => {
@@ -15,4 +16,34 @@ module.exports.onCreateNode = ({ node, actions }) => {
       value: slug,
     })
   }
+}
+
+// gatsby docs: gatsby node api: createPages
+module.exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  // path to template
+  const blogTemplate = path.resolve('./src/templates/blog.js')
+  const res = await graphql(`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  res.data.allMarkdownRemark.edges.forEach(edge => {
+    createPage({
+      component: blogTemplate,
+      path: `/blog/${edge.node.fields.slug}`,
+      context: {
+        slug: edge.node.fields.slug,
+      },
+    })
+  })
 }
