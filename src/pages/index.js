bdc38df0ca7845527e9e97e0e102d76ca95fe2ff
Shaun <shaun37k@gmail.com>
2019-07-31 05:43:18 -0400

    small edits

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,23 +14,23 @@ const IndexPage = () => {
       <p>
         This site was spun up using Gatsby, a framework built upon the React
         library. The purpose of this site is to, initially, tinker around with
-        GraphQL, a query language I used to pull data from local Markdown files
-        to render them as blog posts, as well as to bolster my knowledge of
-        Gatsby in general.
+        GraphQL, a query language I used to pull data from Markdown files to
+        render them as blog posts, as well as to bolster my knowledge of Gatsby
+        in general.
         <br /> <br />
-        After having gotten down the method of rendering Markdown files as blog
-        posts, I installed a Gatsby plugin to communicate with Contentful (a
-        common CMS, a.k.a. Content Management System), and refactored my GraphQL
-        queries to instead interface with Contentful's GraphQL Content API. This
-        allows the end user/client, in this case me, to post blog posts to this
-        site via an easy-to-use UI on Contentful's website.
+        After having gotten down the method of rendering local Markdown files as
+        blog posts, I installed a Gatsby plugin to communicate with Contentful
+        (a common CMS, a.k.a. Content Management System), and refactored my
+        GraphQL queries to instead interface with Contentful's GraphQL Content
+        API. This allows the end user/client, in this case me, to post blog
+        posts to this site via an easy-to-use UI on Contentful's website.
         <br />
         <br />
         Now that the base functionality of this site is working, it's now time
         to make it my own! Later revisions will include sprucing up the CSS of
-        the site, some animations, perhaps a toggle-able hamburger drop-down
-        menu on mobile, sticky header, a legitimate and personalized domain,
-        replace some links with buttons, etc.
+        the site, some animations, perhaps a toggle-able drop-down menu on
+        mobile, sticky header, a legitimate and personalized domain, replace
+        some links with buttons, etc.
       </p>
       <p>
         Need a developer?{' '}
